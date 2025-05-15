78ad4b75b2ce9b0a72a8b064219c7acb65ae92db
beatrizsaraiva <beatri.s@ua.pt>
2025-05-15 01:17:17 +0100

    some dir structure and code for histograms of energy deposition in each detector

---
+++ Utils/scripts/edep_hist_per_detector.py
@@ -0,0 +1,50 @@
+import ROOT
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 edep_hist_per_detector.py <path_to_root_file>")
+    sys.exit(1)
+
+file_path = sys.argv[1]
+file = ROOT.TFile.Open(file_path)
+
+tree = file.Get("edep_Per_Event")
+
+colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2, ROOT.kMagenta]
+detectors = [0, 1, 2, 3]
+
+individual_hst = []
+for i in detectors:
+    hist = ROOT.TH1D(f"hist_energy_dep_{i}", f"energy deposition: detector {i};energy (keV);counts", 300, 0, 600000)
+    tree.Draw(f"detector{i}>>hist_energy_dep_{i}", f"detector{i}>0.0", "goff")
+    individual_hst.append(hist)
+    #save canvas 
+    ind_canvas = ROOT.TCanvas(f"hist for detector {i}", f"Detector {i}", 800, 600)
+    hist.SetLineColor(colors[i])
+    hist.Draw("HIST")
+    ind_canvas.SetLogy()
+    ind_canvas.Update()
+    #c_ind.SaveAs(f"energy_deposition_detector_{det}.png")
+    ind_canvas.SaveAs(f"../output/edep_hist_per_detector/edep_per_detector{i}.png")
+
+
+all_hist = ROOT.TCanvas("all_hist", "all detectors - edep", 1000, 800)
+legend = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)
+
+max_y = max([h.GetMaximum() for h in individual_hst])
+
+frame = all_hist.DrawFrame(0, 0.1, 600000, max_y * 1.5, "energy deposition: all detectors;energy (keV);counts")
+
+
+for det, h in zip(detectors, individual_hst):
+    h.SetLineColor(colors[det])
+    h.Draw("HIST SAME")  #to overlay and shos all together
+    legend.AddEntry(h, f"Detector {det}", "l")
+
+legend.Draw()
+all_hist.SetGrid()
+all_hist.SetLogy() 
+all_hist.Update()
+all_hist.SaveAs("../output/edep_hist_per_detector/energy_deposition_all_detectors.png")
+
+file.Close()
