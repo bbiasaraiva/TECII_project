216f9296ffb0925843049a387db18794a33872d8
beatrizsaraiva <beatri.s@ua.pt>
2025-05-16 11:33:55 +0100

    fix little things

--- Utils/scripts/1_edep_hist_per_detector.py
+++ Utils/scripts/1_edep_hist_per_detector.py
@@ -3,12 +3,12 @@ import sys
 import os
 
 if len(sys.argv) < 2:
-    print("Usage: python3 edep_hist_per_detector.py <path_to_root_file>")
+    print("Usage: python3 1_edep_hist_per_detector.py <path_ROOT_file>")
     sys.exit(1)
 
 file_path = sys.argv[1]
 
-OUTPUT_DIR = '../output/edep_hist_per_detector'
+OUTPUT_DIR = '../output/1_edep_hist_per_detector'
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 file = ROOT.TFile.Open(file_path)
@@ -37,9 +37,7 @@ all_hist = ROOT.TCanvas("all_hist", "all detectors - edep", 1000, 800)
 legend = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)
 
 max_y = max([h.GetMaximum() for h in individual_hst])
-
 frame = all_hist.DrawFrame(0, 0.1, 600000, max_y * 1.5, "energy deposition: all detectors;energy (keV);counts")
-
 
 for det, h in zip(detectors, individual_hst):
     h.SetLineColor(colors[det])
